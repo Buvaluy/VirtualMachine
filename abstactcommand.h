75855c07ef24531f9d7b21260b7108cc2f3307ab
BorisovDmytro <vabidi@yandex.ua>
2016-05-29 13:05:55 +0300

    added new class
    Added abstract class for commands and added compiler

---
+++ abstactcommand.h
@@ -0,0 +1,31 @@
+#ifndef ABSTACTCOMMAND_H
+#define ABSTACTCOMMAND_H
+
+#include <QString>
+
+class AbstactCommand
+{
+private:
+  QString name;
+  QString code;
+  QString arg;
+  QString typeAddr;
+public:
+  AbstactCommand();
+
+  virtual bool execute (QString strArg = "") = 0;
+
+  QString getName() const;
+  void setName(const QString &value);
+
+  QString getCode() const;
+  void setCode(const QString &value);
+
+  QString getArg() const;
+  void setArg(const QString &value);
+
+  QString getTypeAddr() const;
+  void setTypeAddr(const QString &value);
+};
+
+#endif // ABSTACTCOMMAND_H
